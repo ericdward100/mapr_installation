6c19a63af2ecf88ddfa67182a26d53c887f4fce6
Stephen Corbesero <stephen.corbesero@synchronoss.com>
2016-03-22 15:19:15 +0000

    add the users cookbook to dependency list

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,8 @@ description      'Installs/Configures mapr_installation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-%w( build-essential iptables limits ntp sysctl selinux sudo yum ).each do |cb|
+%w( build-essential iptables limits ntp
+    sysctl selinux sudo users yum ).each do |cb|
   depends cb
 end
 
